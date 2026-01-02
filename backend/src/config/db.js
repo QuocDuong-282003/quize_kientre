702e07a2b50014511790fb37fe23c7ded73d7dbf
quocduong <baoduong2972003@gmail.com>
2026-01-02 09:09:45 +0700

    update-url-db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/adaptive_quiz';
-        
+        //const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/adaptive_quiz';
+        const mongoURI = process.env.MONGODB_URI;
+
+        // mongodb+srv://duong:<db_password>@cluster0.djyaprp.mongodb.net/?appName=Cluster0
         const conn = await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -13,7 +15,6 @@ const connectDB = async () => {
         console.log(` MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(` Error: ${error.message}`);
-        // Thoát chương trình với mã lỗi nếu không kết nối được DB
         process.exit(1);
     }
 };
